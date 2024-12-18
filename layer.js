926df91d6c8669057495e18cde385b016f87f647
8tsmith11 <8tsmith11@gmail.com>
2024-12-17 21:58:49 -0500

    layer

---
+++ layer.js
@@ -0,0 +1,12 @@
+export class Layer {
+    constructor(layerID, depth, tileID) {
+        this.layerID = layerID;
+        this.tileGrid = this.createGrid(depth, tileID);
+    }
+
+    createGrid(depth, tileID) {
+        return Array.from({ length: depth }, () => 
+            Array.from({ length: 50 }, () => tileId)
+        );
+    }
+}
